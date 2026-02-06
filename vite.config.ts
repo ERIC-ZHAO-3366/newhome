92849453c80ab40e63b9445de3950e2918660d0b
ERIC ZHAO <3177261635@qq.com>
2026-02-06 23:24:19 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://musicapi.shinji.ren',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [vue({
     template: {
       compilerOptions: {
